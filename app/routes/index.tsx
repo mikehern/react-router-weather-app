7e956606dd634e9d8d41f5c1b8aad881b0d86ca1
Mike Hernandez <mkhern@gmail.com>
2025-09-07 16:01:07 -0400

    feat: add client-side geolocation data fetch

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,14 +1,31 @@
 import type { Route } from "./+types/index";
-import { Outlet, redirect } from "react-router";
+import { useLoaderData, type LoaderFunctionArgs } from "react-router";
 
-export function loader({ request }: Route.LoaderArgs) {
+export function loader({ request }: LoaderFunctionArgs) {
   return { message: "Mobile First Response Weather App" };
 }
 
+export async function clientLoader({
+  params,
+  serverLoader,
+}: Route.ClientLoaderArgs) {
+  const serverData = await serverLoader();
+  const position = await new Promise<GeolocationPosition>((resolve, reject) =>
+    navigator.geolocation.getCurrentPosition(resolve, reject)
+  );
+  const { latitude, longitude } = position.coords;
+  return { ...serverData, coords: { latitude, longitude } };
+}
+
+// react-router boilerplate for client-side data fetching during hydration
+clientLoader.hydrate = true;
+
 export default function Home({ loaderData }: Route.ComponentProps) {
+  const { message, coords } = useLoaderData();
   return (
     <div className="max-w-5xl mx-auto my-8">
-      <h1 className="text-3xl">{loaderData.message}</h1>
+      <h1 className="text-3xl">{message}</h1>
+      <h2 className="text-xl text-wrap">{JSON.stringify(coords)}</h2>
       <div className="flex flex-col bg-gray-100 w-full items-center">
         Content
       </div>
